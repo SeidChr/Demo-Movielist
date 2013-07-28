e04cb621a587b9135e25fd39c3290c3140cb0eb5
Christian Seidlitz <seidchr@gmail.com>
2013-07-28 19:22:12 +0200

    Added Configuration Class

---
+++ Movielist/Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Movielist
+{
+    static class Config
+    {
+        private static IEnumerable<string> endings;
+
+        static Config()
+        {
+            var configDocument = XDocument.Load("Config.xml");
+            endings = configDocument.Element("FileEndings").Elements("Ending").Select(e => e.Value);
+        }
+
+        public static IEnumerable<string> Endings
+        {
+            get 
+            {
+                return endings;
+            }
+        }
+    }
+}
