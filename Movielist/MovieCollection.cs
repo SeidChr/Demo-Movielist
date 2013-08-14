efcb7e17f737350bc6fc39adb2284cefbb6bcada
Christian Seidlitz <seidchr@gmail.com>
2013-08-15 00:56:16 +0200

    correctly filling movie store

--- Movielist/MovieCollection.cs
+++ Movielist/MovieCollection.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using XEO.Core.Text;
 
 namespace Movielist
 {
@@ -12,9 +14,16 @@ namespace Movielist
     {
         private Dictionary<string, Movie> movieStore = new Dictionary<string, Movie>();
 
-        public void Add(FileInfo file)
+        public void Add(FileInfo file, string location)
         {
-            
+            var movie = new Movie();
+            movie.Location = location;
+            var match = Regex.Match(file.Name, @"^(?<name>.*?)\s*\[(?<tag>[^,]*)(,\s?(?<tag>[^,]*))*\]\.\w+$");
+            movie.Name = match.Groups["name"].Value;
+            movie.Tags = match.Groups["tag"].Captures.Cast<Capture>().Select(c => c.Value).ToList();
+            string key = location + file.FullName.Substring(1);
+
+            movieStore.Add(key, movie);
         }
 
         public IEnumerator<Movie> GetEnumerator()
